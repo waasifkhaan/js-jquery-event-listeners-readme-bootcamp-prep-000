4bfc6cc86a26dfccf1d677e450fcbaf895de13d1
Wasif Khan <waasif.khaan@gmail.com>
2018-04-18 16:52:45 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -3,8 +3,13 @@ function getIt(){
 $('p').on('click')
 return alert("Hey!")
 }
+function frameIt(){
+  $('img').css("tasty").on('load' )
+  
+}
 $(document).ready(function(){
 getit()
+frameIt()
 // call functions here
 
 });
