e1614e390858c8f7cacd5c25dd2abce3830d956a
Wasif Khan <waasif.khaan@gmail.com>
2018-04-18 18:49:46 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,37 @@
 //define functions here
+function getIt(){
+$('p').on('click', function (){
+return alert("Hey!")})
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+  $('img').addClass('tasty')
+  }
+)
+
+}
+
+function pressIt(){
+$('#typing').on('keydown', function(key){
+  if(key.which == 71){
+    return  alert('G was pressed');
+}
+
+})}
+
+
+function submitIt(){
+  $("form").on("submit", function() {
+    return alert('Your form is going to be submitted now.');
+})}
+
 
 $(document).ready(function(){
-
+//getIt()
+//frameIt()
+submitIt()
 // call functions here
+
 
 });
