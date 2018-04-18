ccd5c33cc794b7c4c797b823d4bca93736aa23c2
Wasif Khan <waasif.khaan@gmail.com>
2018-04-18 17:29:49 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -3,10 +3,18 @@ function getIt(){
 $('p').on('click')
 return alert("Hey!")
 }
+function pressIt(){
+$('#typing').on('keydown' function(key){
+  if(key.which == 83){
+      return alert('G was pressed');
 
+})
+
+}}
 $(document).ready(function(){
 
 getIt()
+pressIt()
 
 // call functions here
 
